c0e4b0bc8725bf16ac17176102924b69f2ebeefd
iparusov <iparusov@yandex.ru>
2021-02-23 18:28:37 +0300

    начать и завершить проект

---
+++ pages/index.js
@@ -0,0 +1,28 @@
+let editButton = document.querySelector(".profile__button-edit");
+let closeButton = document.querySelector(".popup__close");
+let popup = document.querySelector(".popup");
+let profileName = document.querySelector(".profile__name");
+let profileEmployment = document.querySelector(".profile__employment");
+let popupName = document.querySelector(".popup__name");
+let popupEmployment = document.querySelector(".popup__employment");
+let saveButton = document.querySelector(".popup__submit");
+
+function closePopup () {
+    popup.classList.remove("popup_opened");
+}
+
+editButton.addEventListener("click", function () {
+    popupName.value = profileName.textContent;
+    popupEmployment.value = profileEmployment.textContent;
+    popup.classList.add("popup_opened");
+    console.log(popup.classList)
+});
+closeButton.addEventListener("click", closePopup);
+
+saveButton.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    profileName.textContent = popupName.value;
+    profileEmployment.textContent = popupEmployment.value;
+    closePopup ();
+})
+
